0b3fe06bcd7a24a55d699343563345bac08f7196
hoangduy221193 <hoangduy221193@gmail.com>
2024-03-16 15:36:24 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -34,6 +34,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span
+          >
+        </router-link>
       </div>
     </div>
   </div>
@@ -120,7 +130,7 @@ export default {
 </script>
 <style scoped>
 .page {
-text-align: left;
-max-width: 750px;
+  text-align: left;
+  max-width: 750px;
 }
 </style>
