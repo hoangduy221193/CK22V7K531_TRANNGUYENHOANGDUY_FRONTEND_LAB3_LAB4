fd6b0d2e6ba416b19e3e0158ab1ce8db17092b72
hoangduy221193 <hoangduy221193@gmail.com>
2024-03-17 08:58:09 +0700

    Tao trang cap nhat lien he

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -29,7 +29,7 @@ export default {
         async addContact() {
             try {
                 await ContactService.create(this.contact);
-                this.message = 'Liên hệ được thêm thành công.';
+                alert("Liên hệ được thêm thành công");
                 this.$router.push({ name: 'contactbook' });
             } catch (err) {
                 console.log(err);
